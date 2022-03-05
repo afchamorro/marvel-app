a7c099111b5a0a9a870cb8af82c9a7fd3bbfa12f
afdezchamorro <afernandez@freepik.com>
2022-03-05 11:04:00 +0100

    Application file

--- app/src/main/java/com/acoders/marvelfanbook/MarvelFanBookApplication.kt
+++ app/src/main/java/com/acoders/marvelfanbook/MarvelFanBookApplication.kt
@@ -4,4 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MarvelFanBookApplication: Application()
+class MarvelFanBookApplication : Application() {
+    //TODO
+}

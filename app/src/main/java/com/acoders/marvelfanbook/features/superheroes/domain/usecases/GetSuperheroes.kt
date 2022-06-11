e0682455ed3ad7b73cb17dd11f0fe0218a4cd70e
Antonio Fernández Chamorro <igualachamorro@gmail.com>
2022-06-11 20:26:51 +0200

    fix(merge): solved conflict merging

--- app/src/main/java/com/acoders/marvelfanbook/features/superheroes/domain/usecases/GetSuperheroes.kt
+++ app/src/main/java/com/acoders/marvelfanbook/features/superheroes/domain/usecases/GetSuperheroes.kt
@@ -1,11 +1,9 @@
 package com.acoders.marvelfanbook.features.superheroes.domain.usecases
 
-import com.acoders.marvelfanbook.core.exception.Failure
-import com.acoders.marvelfanbook.core.functional.Either
-import com.acoders.marvelfanbook.core.interactor.UseCase
-import com.acoders.marvelfanbook.data.remote.schemes.common.PaginatedWrapper
-import com.acoders.marvelfanbook.features.superheroes.domain.SuperheroesRepository
-import com.acoders.marvelfanbook.features.superheroes.domain.models.Superhero
+import com.acoders.marvelfanbook.features.superheroes.domain.repository.SuperheroesRepository
+import com.acoders.marvelfanbook.features.superheroes.presentation.model.SuperheroView
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetSuperheroes @Inject constructor(private val repository: SuperheroesRepository) {

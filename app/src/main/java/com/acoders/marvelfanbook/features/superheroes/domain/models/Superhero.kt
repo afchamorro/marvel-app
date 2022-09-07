187cfef864959b2723ba5d1edc9a87435213f8d2
Antonio Fernández Chamorro <igualachamorro@gmail.com>
2022-09-07 18:42:00 +0200

    refactor(#35): remove old code

--- app/src/main/java/com/acoders/marvelfanbook/features/superheroes/domain/models/Superhero.kt
+++ app/src/main/java/com/acoders/marvelfanbook/features/superheroes/domain/models/Superhero.kt
@@ -2,7 +2,6 @@ package com.acoders.marvelfanbook.features.superheroes.domain.models
 
 import com.acoders.marvelfanbook.features.common.domain.models.Thumbnail
 import com.acoders.marvelfanbook.features.superheroes.framework.database.SuperHeroEntity
-import com.acoders.marvelfanbook.features.superheroes.presentation.model.DescriptionView
 import com.acoders.marvelfanbook.features.superheroes.presentation.model.SuperheroView
 
 data class Superhero(
@@ -13,8 +12,6 @@ data class Superhero(
 ) {
 
     fun toPresentationModel() = SuperheroView(id, name, description, thumbnail.toPresentationModel())
-
-    fun toDescriptionView() = DescriptionView(description)
 
     fun toEntityModel() = SuperHeroEntity(id, name, description, thumbnail.path, thumbnail.extension)
 }
